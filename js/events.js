08d16e3c3b50a4c06443666c86cfea40888f9282
Olivia Kesler <oakesler@protonmail.ch>
2019-03-24 22:11:32 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
-
-$(document).ready(function(){
-
-// call functions here
+$(document).ready(function() {
 
 });
+
+
+function getIt() {
+  $('p').on("click", function(){
+  alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('#tasty').on('load', function(){
+});
+}
+
+function pressIt() {
+  $(input).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+}
+
+
+function submitIt() {
+  
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
+}
+
+  
